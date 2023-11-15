0f14c9080ef865353b43610586d2306fc1bf8f6c
limaismael8901 <limaismael8901@gmail.com>
2023-11-15 06:44:53 -0300

    add test

---
+++ Estruturas de Repetição e Recursividade/Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estruturas_de_Repetição_e_Recursividade
+{
+    internal class Test
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            while (num <= 10)
+            {
+                int i = 1;
+                int x = num;
+                
+                while (i <= 10)
+                {
+                    int result = i * x;
+                    Console.WriteLine($"{x} * {i} = {result}");
+                    i++;
+                    
+                }
+                Console.WriteLine("-------------------");
+                num++;
+            }
+        }
+    }
+}
